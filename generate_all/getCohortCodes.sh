ecd8e13c8e0a1d8b704f114d80b6ce4e838d30af
Nathan Dunn <ndunn@me.com>
2020-08-19 13:07:46 -0700

    the analysis data

--- generate_all/getCohortCodes.sh
+++ generate_all/getCohortCodes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl 'https://raw.githubusercontent.com/ucscXena/XenaGoWidget/develop/src/data/defaultDatasetForGeneset.json' | jq 'keys'  | cut -f2 -d '(' | cut -f1 -d ')' | grep -v "\[" | grep -v "\]" | grep -v Breast | tee  cohortCodes.txt
+#curl 'https://raw.githubusercontent.com/ucscXena/XenaGoWidget/develop/src/data/defaultDatasetForGeneset.json' | jq 'keys'  | cut -f2 -d '(' | cut -f1 -d ')' | grep -v "\[" | grep -v "\]" | grep -v Breast | tee  cohortCodes.txt
 
 CODES=`cat cohortCodes.txt`
 
@@ -12,3 +12,19 @@ do
 done
 
 gunzip *.gz
+
+echo "ARGS: $1 $2"
+
+
+
+
+for COHORT in $CODES
+do
+   echo "PROCESSING $COHORT"
+
+#   wget "https://xenago.xenahubs.net/download/expr_tpm/TCGA-${COHORT}_tpm_tab.tsv.gz"
+
+  Rscript ../analysis-wrapper.R $1 TCGA-${COHORT}_tpm_tab.tsv geneset_output_${COHORT}.tsv
+
+   echo "DONE processing $COHORT -> ${geneset_output_${COHORT}.tsv}"
+done
