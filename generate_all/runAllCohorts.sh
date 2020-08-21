9ad3aa0e00fecf1a1ae4bf7f1d451b4739980a72
Nathan Dunn <ndunn@me.com>
2020-08-21 08:47:46 -0700

    fixes #2

---
+++ generate_all/runAllCohorts.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#curl 'https://raw.githubusercontent.com/ucscXena/XenaGoWidget/develop/src/data/defaultDatasetForGeneset.json' | jq 'keys'  | cut -f2 -d '(' | cut -f1 -d ')' | grep -v "\[" | grep -v "\]" | grep -v Breast | tee  cohortCodes.txt
+
+echo "ARGS: $1 $2"
+
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters $# not 1.  Usage: ./runAllCohorts <gmt_file>.tsv"
+	exit
+fi
+
+
+CODES=`cat cohortCodes.txt`
+
+echo $CODES
+
+for COHORT in $CODES
+do
+   echo "EXECUTING $COHORT"
+#   wget "https://xenago.xenahubs.net/download/expr_tpm/TCGA-${COHORT}_tpm_tab.tsv.gz"
+#   echo "Rscript ../analysis-wrapper.R  $1  TCGA-${COHORT}_tpm_tab.tsv $1-genesets-${COHORT}.tsv"
+   Rscript ../analysis-wrapper.R  $1  TCGA-${COHORT}_tpm_tab.tsv $1-genesets-${COHORT}.tsv
+done
+
+#gunzip *.gz
+
+
+
+
+
+#for COHORT in $CODES
+#do
+#   echo "PROCESSING $COHORT"
+
+##   wget "https://xenago.xenahubs.net/download/expr_tpm/TCGA-${COHORT}_tpm_tab.tsv.gz"
+
+#  Rscript ../analysis-wrapper.R $1 TCGA-${COHORT}_tpm_tab.tsv geneset_output_${COHORT}.tsv
+
+#   echo "DONE processing $COHORT -> geneset_output_${COHORT}.tsv"
+#done
