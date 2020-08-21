6188f863ece85c2decb5c6f85049684da503045a
Nathan Dunn <ndunn@me.com>
2020-08-21 08:32:46 -0700

    fixes #4

--- generate_all/convert_uniprots_to_genes.py
+++ generate_all/convert_uniprots_to_genes.py
@@ -22,22 +22,17 @@ def individual_conversion(gene_batch):
     return response.decode('utf-8')
 
 
-def do_conversion(genes):
+def do_conversion(input_genes):
     counter = 0
     gene_map = {}
-    gene_length = len(genes)
+    gene_length = len(input_genes)
     print("converting genes " + str(gene_length))
     while counter < gene_length:
-        # TODO: this is a bug  as we need to conver the string
-        # assert 0 == 1
-        converted = individual_conversion(genes[counter:counter + batch_size])
-        # print("converted: "+str(converted))
+        converted = individual_conversion(input_genes[counter:counter + batch_size])
         for gene_entry in converted.split("\n"):
             gene_array = gene_entry.split("\t")
             if not gene_array[0].startswith("From", 0) and len(gene_array) > 1:
                 gene_map[gene_array[0]] = gene_array[1]
-        # return_genes
-        # TODO: add to return genes
         counter += batch_size
         print("at : " + str(counter / gene_length * 100.0) + "%")
 
@@ -66,17 +61,17 @@ def convert_gmt_uniprots(input_file_name, conversion_map):
             gene_set_goid = stripped_tabbed_line[0].split("%BP%")[1]
             output_file.write(gene_set_name + "\t" + gene_set_goid + "\t")
             replaced_genes = do_replace_genes(input_genes=stripped_tabbed_line[1:], conversion_map=conversion_map)
-            output_file.write(' '.join(replaced_genes) + "\n")
+            output_file.write('\t'.join(replaced_genes) + "\n")
     output_file.close()
 
 
 def get_genes_from_gmt(input_file_name):
-    genes = []
+    retrieved_genes = []
     with open(input_file_name, "r") as a_file:
         for line in a_file:
             stripped_tabbed_line = line.strip().split("\t")
-            genes.append(' '.join(stripped_tabbed_line[1:]).replace('UniProtKB:', '').strip().split(' '))
-    return genes
+            retrieved_genes.append(' '.join(stripped_tabbed_line[1:]).replace('UniProtKB:', '').strip().split(' '))
+    return retrieved_genes
 
 
 print("input arguments: " + str(sys.argv))
@@ -103,7 +98,7 @@ unique_genes.sort()
 
 print("UNIQUE GENES: " + str(len(unique_genes)))
 
-flattened_conversion_map = do_conversion(genes=unique_genes)
+flattened_conversion_map = do_conversion(input_genes=unique_genes)
 
 print("KEYS IN CONVERSION MAP: " + str(len(flattened_conversion_map.keys())))
 
